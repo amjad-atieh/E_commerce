2d8dbbe55bc28245977f73e38f94d48ae16938bc
amjad-atieh <amjdsalh@gmail.com>
2026-05-06 13:43:36 +0300

    database connection information is not managed by .env variables

--- backend/postgres_init.js
+++ backend/postgres_init.js
@@ -1,13 +1,15 @@
 import pgp from "pg-promise";
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
 
 const pgInstance = pgp();
 
 const db = pgInstance({
-  host: "localhost",
-  port: 5432,
-  database: "e_commerce",
-  user: "e_commer",
-  password: "password123",
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || "e_commerce",
+  user: process.env.DB_USER || "e_commer",
+  password: process.env.DB_PASSWORD || "password123",
 });
 
 export default db;
